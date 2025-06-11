3e4c73df6cbc57dcedcf58c35f387b3d47b9b286
LuckyGuy-777 <116659339+LuckyGuy-777@users.noreply.github.com>
2025-06-11 19:48:38 +0900

    25.6.11 7:48 commit
    sec 162

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -4,27 +4,36 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
-	private Integer id;
 	
-	//이름은 최소 2글자. @Size
-	@Size(min=2, message = "이름은 적어도 2글자 이어야만 합니다.")
-	@JsonProperty("user_name") // 필드명 커스터마이징
-	private String name;
+	@Id
+	@GeneratedValue
+	private Integer id;
 	
 	// 생일은 항상 과거날짜로. @Past 
 	@Past(message = "생일 날짜는, 반드시 과거의 날짜이어야만 합니다.")
 	@JsonProperty("birth_date")
 	private LocalDate birthdate;
 	
+	//이름은 최소 2글자. @Size
+	@Size(min=2, message = "이름은 적어도 2글자 이어야만 합니다.")
+	@JsonProperty("user_name") // 필드명 커스터마이징
+	private String name;
+	
+
+	
 	public User(Integer id, String name, LocalDate birthdate) {
 		super();
 		this.id = id;
-		this.name = name;
 		this.birthdate = birthdate;
+		this.name = name;
 	}
 
 	public Integer getId() {
